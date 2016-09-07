f4ec0ae3325831ef0ff96a2d73c84d0a59d2f4d5
Waihon Yew <yewwaihon@gmail.com>
2016-09-07 20:02:49 +0800

    Complete 6. Methods

--- crowdfund.rb
+++ crowdfund.rb
@@ -1,15 +1,16 @@
+def project_info(name, funding=1000)
+  "#{name} has $#{funding} in funding."
+end
+
 project1 = "Project ABC"
-funding1 = 1000
-puts "#{project1} has $#{funding1} in funding."
+puts project_info(project1)
 
 project2 = "Project LMN"
 funding2 = 2500
-puts " #{project2}: $#{funding2} ".center(50, "*")
+puts project_info(project2, funding2)
 
 project3 = "Project XYZ"
 funding3 = 3800
-puts "#{project3.ljust(50, ".")} $#{funding3}"
-
-
+puts project_info(project3, funding3)
 
 puts "Projects:\n\t#{project1}\n\t#{project2}\n\t#{project3}"
