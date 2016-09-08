58c964ef696f87410c203008984db8467dd15827
Waihon Yew <yewwaihon@gmail.com>
2016-09-08 14:37:52 +0800

    Complete 7. Classes

--- crowdfund.rb
+++ crowdfund.rb
@@ -1,16 +1,36 @@
+class Project
+  def initialize(name, funding, target_funding)
+    @name = name
+    @funding = funding
+    @target_funding = target_funding
+  end
+
+  def add_fund(funding=25)
+    @funding += funding
+    puts "#{@name} got more funds!"
+  end
+
+  def remove_fund(funding=15)
+    @funding -= funding
+    puts "#{@name} lost some funds!"
+  end
+
+  def to_s
+    "#{@name} has $#{@funding} in funding towards a goal of $#{@target_funding}."
+  end
+end
+
 def project_info(name, funding=1000)
   "#{name} has $#{funding} in funding."
 end
 
-project1 = "Project ABC"
-puts project_info(project1)
+project1 = Project.new("Project LMN", 500, 3000)
+puts project1
 
-project2 = "Project LMN"
-funding2 = 2500
-puts project_info(project2, funding2)
+project2 = Project.new("Project XYZ", 25, 75)
+puts project2
 
-project3 = "Project XYZ"
-funding3 = 3800
-puts project_info(project3, funding3)
-
-puts "Projects:\n\t#{project1}\n\t#{project2}\n\t#{project3}"
+project1.remove_fund
+project2.add_fund
+puts project1
+puts project2
