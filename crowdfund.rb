438068ee7003c6657802580a2d746a47abbf8635
Waihon Yew <yewwaihon@gmail.com>
2016-09-24 18:27:39 +0800

    Complete 19. Input/Output

--- crowdfund.rb
+++ crowdfund.rb
@@ -1,13 +1,21 @@
 require_relative 'fundrequest'
 require_relative 'project'
 
-project1 = Project.new("Project LMN", 3000, 500)
-project2 = Project.new("Project XYZ", 500, 75)
-project3 = Project.new("Project SAAS", 1000)
-
 vc_friendly = FundRequest.new("VC-Friendly Start-up Projects")
-vc_friendly.add_project(project1)
-vc_friendly.add_project(project2)
-vc_friendly.add_project(project3)
-vc_friendly.request_funding(5)
-vc_friendly.print_stats
+vc_friendly.load_projects(ARGV.shift || "projects.csv")
+
+loop do
+  puts "How many rounds of funding requests? ('quit' to exit)"
+  answer = gets.chomp.downcase
+  case answer
+  when /^\d+$/  # 1 or more digits
+    vc_friendly.request_funding(Integer(answer))
+  when 'quit', 'exit'
+    vc_friendly.print_stats
+    break
+  else
+    puts "Please enter a number or 'quit'"
+  end
+end
+
+vc_friendly.save_projects
--- fundrequest.rb
+++ fundrequest.rb
@@ -2,6 +2,7 @@ require_relative 'die'
 require_relative 'project'
 require_relative 'funding_round'
 require_relative 'pledge_pool'
+require 'csv'
 
 class FundRequest
   attr_reader name
@@ -44,24 +45,69 @@ class FundRequest
       puts "$#{project.total_pledges} in total pledges"
     end
 
-    fully_funded, under_funded = @projects.partition { |project| project.fully_funded? }
+    #fully_funded, under_funded = @projects.partition { |project| project.fully_funded? }
 
     puts "\n#{@name} Statistics:"
 
-    puts "\n#{fully_funded.size} fully-funded projects:"
-    fully_funded.each do |project|
-      print_name_and_funding(project)
-    end
+    # puts "\n#{fully_funded_projects.size} fully-funded projects:"
+    # fully_funded_projects.each do |project|
+    #   print_name_and_funding(project)
+    # end
+    print_fully_funded_projects
 
-    puts "\n#{under_funded.size} under-funded projects:"
-    under_funded.sort.each do |project|
-      print_name_and_funding(project)
+    # puts "\n#{under_funded_projects.size} under-funded projects:"
+    # under_funded_projects.sort.each do |project|
+    #   print_name_and_funding(project)
+    # end
+    print_under_funded_projects
+  end
+
+  def load_projects(from_file)
+    CSV.foreach(from_file) do |row|
+      # funding (3rd attribute) is optional
+      if row[2].nil?
+        project = Project.new(row[0], row[1].to_i)
+      else
+        project = Project.new(row[0], row[1].to_i, row[2].to_i)
+      end
+      add_project(project)
+    end
+  end
+
+  def fully_funded_projects
+    @projects.select { |project| project.fully_funded? }
+  end
+
+  def under_funded_projects
+    @projects.reject { |project| project.fully_funded? }
+  end
+
+  def save_projects(to_file="projects.txt")
+    File.open(to_file, "w") do |file|
+      print_under_funded_projects(file)
     end
   end
 
   private
 
-    def print_name_and_funding(project)
-      puts "#{project.name} (#{project.funding_needed})"
+    def print_fully_funded_projects(file=$stdout)
+      prefix = "\n" if file == $stdout
+      file.puts "#{prefix}#{fully_funded_projects.size} fully-funded projects:"
+      fully_funded_projects.each do |project|
+        print_name_and_funding(project, file)
+      end
     end
+
+    def print_under_funded_projects(file=$stdout)
+      prefix = "\n" if file == $stdout
+      file.puts "#{prefix}#{under_funded_projects.size} under-funded projects:"
+      under_funded_projects.sort.each do |project|
+        print_name_and_funding(project, file)
+      end
+    end
+
+    def print_name_and_funding(project, file=$stdout)
+      file.puts "#{project.name} (#{project.funding_needed})"
+    end
+
 end
